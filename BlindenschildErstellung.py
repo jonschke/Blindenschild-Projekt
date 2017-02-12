b525e9f061a8d60cd5e70dd0396399a16095226b
jonschke <jonathan.blaschke@posteo.de>
2017-02-12 14:56:32 +0100

    first commit

--- BlindenschildErstellung.py
+++ BlindenschildErstellung.py
@@ -6,10 +6,17 @@ ui = app.userInterface;
 BuchstabenDictionary = {}
 Schild = "12"
 Schildformat = "42,4"
+
 design = app.activeProduct
 root = design.rootComponent
 occs = root.occurrences
+def DegtoRad(Deg):
+	return Deg*math.pi/180
 
+def floatConversion (number):
+    return float(number.replace(",", "."))
+
+SchildformatAlsFloat = floatConversion(Schildformat)/10
 xVerschiebung = 0
 AktivesProjekt = app.data.activeProject
 WurzelOrdner = AktivesProjekt.rootFolder.dataFolders
@@ -18,14 +25,38 @@ PyramidenschriftOrdner = BuchstabenOrdner.dataFolders.itemByName("16mm ({}) Einf
 BrailleOrdner = BuchstabenOrdner.dataFolders.itemByName("Braille {}".format(Schildformat))
 Pyramidenbuchstaben = PyramidenschriftOrdner.dataFiles
 Braillebuchstaben = BrailleOrdner.dataFiles
-ZahlZuBuchstabe = {1:"A", 2:"B", 3:"C", 4:"D", 5:"E", 6:"F", 7:"G", 8:"H", 9:"I", 0 : "J" }
-BuchstabeZuZahl = {"A":1, "B":2, "C":3, "D":4, "E":5, "F":6, "G":7, "H": 8, "I": 9, "J":0 }
-ZahlenModus = 0
+ZahlZuBuchstabe = {"1":"A", "2":"B", "3":"C", "4":"D", "5":"E", "6":"F", "7":"G", "8":"H", "9":"I", "0": "J" }
+BuchstabeZuZahl = {"A":"1", "B":"2", "C":"3", "D":"4", "E":"5", "F":"6", "G":"7", "H": "8", "I": "9", "J":"0" }
+SchildBreite = adsk.core.ValueInput.createByReal(20)
+
+
 Ursprung = adsk.core.Point3D.create()
-XVektor = adsk.core.Vektor3D.create(1,0,0)
+obererPunkt = adsk.core.Point3D.create(0,float(SchildformatAlsFloat)/2,0)
+untererPunkt = adsk.core.Point3D.create(0,float(SchildformatAlsFloat)/2-0.2,0)
+XVektor = adsk.core.Vector3D.create(1,0,0)
 WinkelfuerDrehung = -50
-radfuerDrehung = math.pi*WinkelfuerDrehung/180
+WinkelfueSeite1= 70
+WinkelfueSeite2=-35
+radfuerDrehung = DegtoRad(WinkelfuerDrehung)
+radfuerSeite1 = DegtoRad(WinkelfueSeite1)
+radfuerSeite2 = DegtoRad(WinkelfueSeite2)
 ##### KomponentenErstellung ####
+BasisSkizze = root.sketches.add(root.yZConstructionPlane)
+BasisKurven = BasisSkizze.sketchCurves
+BasisLines = BasisKurven.sketchLines
+Kurve1 = BasisKurven.sketchArcs.addByCenterStartSweep(Ursprung, obererPunkt, radfuerSeite1)
+Kurve2 = BasisKurven.sketchArcs.addByCenterStartSweep(Ursprung,Kurve1.startSketchPoint, radfuerSeite2)
+Kurven = BasisSkizze.findConnectedCurves(Kurve1)
+Offset = BasisSkizze.offset(Kurven, untererPunkt, 0.2)
+BasisLines.addByTwoPoints(Kurve1.endSketchPoint, Offset.item(1).endSketchPoint)
+BasisLines.addByTwoPoints(Kurve2.startSketchPoint, Offset.item(0).startSketchPoint)
+Profile = BasisSkizze.profiles.item(0)
+RootExtrudes = root.features.extrudeFeatures
+RootExtrudeInput = rootExtrudes.createInput(Profile, 3)
+RootExtrudeInput.setSymmetricExtent(SchildBreite ,True)
+Extrusion = RootExtrudes.add(RootExtrudeInput)
+
+exit()
 
 MoveMatrix = adsk.core.Matrix3D.create()
 PyramidenOcc = occs.addNewComponent(MoveMatrix)
@@ -43,7 +74,9 @@ BrailleComponent.name = "Braille Buchstaben"
 PyramidenOccs = PyramidenComponent.occurrences
 BrailleOccs = BrailleComponent.occurrences
 
+
 def stringtoDateinamenListe(beschriftung, schildformat):
+	ZahlenModus = 0
 	brailleDateinamenListe = []
 	pyramidenDateinamenListe = []
 	for char in beschriftung:
@@ -78,10 +111,16 @@ def stringtoDateinamenListe(beschriftung, schildformat):
 def makeitemNamenDictionary (dateien):
 	itemNamenDictionary = {}
 	for n in range(0, len(dateien)):
-		dateiName = datei.item(n).name
-		datei = datei.item(n)
+		dateiName = dateien.item(n).name
+		datei = dateien.item(n)
 		itemNamenDictionary.update({dateiName : datei})
 	return itemNamenDictionary
+
+
+
+
+
+
 
 PyramidenDateinamenListe, BrailleDateinamenListe = stringtoDateinamenListe(Schild, Schildformat)
 
@@ -96,7 +135,7 @@ Vektor = adsk.core.Vector3D.create(0,0,0)
 for DateiName in PyramidenDateinamenListe:
     Datei = PyramidenDictionary[DateiName]
     Buchstabe = PyramidenOccs.addByInsert(Datei, MoveMatrix, True)
-    halbeBuchstabenBreite = Buchstabe.component.parentDesign.allParameters.itemByName("breite").value
+    halbeBuchstabenBreite = Buchstabe.component.parentDesign.allParameters.itemByName("breite").value/2
     xVerschiebung += halbeBuchstabenBreite
     Vektor.x = xVerschiebung
     MoveMatrix.translation = Vektor
@@ -110,20 +149,21 @@ Vektor.x = -Vektor.x/2
 MoveMatrix.translation = Vektor
 PyramidenOcc.transform = MoveMatrix
 design.snapshots.add()
-exit()
 
-Vektor = adsk.core.Vector3D.create(0.66,0,0)
+Vektor.x = 0.33
 #???
-MoveMatrix = adsk.core.Matrix3D.create(Vektor)
+MoveMatrix = adsk.core.Matrix3D.create()
+MoveMatrix.translation = Vektor
 
 for DateiName in BrailleDateinamenListe:
-	Datei = BrailleDictionary
+	Datei = BrailleDictionary[DateiName]
 	Buchstabe = BrailleOccs.addByInsert(Datei, MoveMatrix, True)
-	Vektor.x += 0.33
+	Vektor.x += 0.66
 	MoveMatrix.translation = Vektor
 
-Vektor.x = -Vektor.x/2
-MoveMatrix.translation = Vektor
+Vektor.x = -(Vektor.x -0.33)/2
 MoveMatrix.setToRotation(radfuerDrehung, XVektor, Ursprung)
+MoveMatrix.translation = Vektor
 BrailleOcc.transform = MoveMatrix
 design.snapshots.add()
+# Blindenschild-Projekt
